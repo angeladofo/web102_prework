3a8faec6454336237a94ffef922f92ba7f6806e0
angeladofo <78775923+angeladofo@users.noreply.github.com>
2024-08-05 12:31:25 -0400

    modified my code to display the same UI as exemp

--- index.js
+++ index.js
@@ -40,17 +40,10 @@ function addGamesToPage(games) {
     newDiv.innerHTML = `<img class="game-img" src="${game.img}" />
     <div>${game.name}</div>
     <div>${game.description}</div>`;
+    console.log(newDiv)
     allGames.appendChild(newDiv)
   });
 
-  // add the class game-card to the list
-
-  // set the inner HTML using a template literal to display some info
-  // about each game
-  // TIP: if your images are not displaying, make sure there is space
-  // between the end of the src attribute and the end of the tag ("/>")
-
-  // append the game to the games-container
 }
 addGamesToPage(GAMES_JSON)
 
